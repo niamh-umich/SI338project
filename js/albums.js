dd2b07e91f454355060e5d1da8d25f37e39d3f04
duffyn25 <niamh.duffy25@mail.dcu.ie>
2025-11-18 12:51:48 -0500

    album covers

--- js/albums.js
+++ js/albums.js
@@ -1,27 +1,27 @@
 const albums = {
   dawnfm: {
-    title: "Dawn FM",
+    title: "You Have Selected Dawn FM",
     description: "“Dawn FM” is a concept album framed as a surreal radio broadcast guiding listeners through a liminal, purgatory-like state. It blends synth-pop, disco, and electronic influences to explore themes of reflection, regret, and spiritual transition.",
     cover: "images/dawnfm.jpg",
     alt: "Dawn FM album cover",
     spotify: "https://open.spotify.com/embed/album/2nLOHgzXzwFEpl62zAgCEC?utm_source=generator&theme=0"
   },
   starboy: {
-    title: "Starboy",
+    title: "You Have Selected Starboy",
     description: "“Starboy” marks a transformation in The Weeknd's persona, diving into fame, excess, and self-destruction with slick synth wave production. The album embraces a more futuristic, pop leaning sound while still carrying his dark R&B edge.",
     cover: "images/starboy.jpg",
     alt: "Starboy album cover",
     spotify: "https://open.spotify.com/embed/album/2ODvWsOgouMbaA5xf0RkJe?utm_source=generator&theme=0"
   },
   kissland: {
-    title: "Kiss Land",
+    title: "You Have Selected Kiss Land",
     description: "“Kiss Land” is a cinematic, neon-lit debut studio album inspired by isolation, culture shock, and life on the road. Its dark, atmospheric production reflects The Weeknd's emotional disconnect and anxieties during sudden fame.",
     cover: "images/kissland.jpg",
     alt: "Kiss Land album cover",
     spotify: "https://open.spotify.com/embed/album/3hhDpPtCFuQbppwYgsVhMO?utm_source=generator&theme=0"
   },
   trilogy: {
-    title: "Trilogy",
+    title: "You Have Selected Trilogy",
     description: "“Trilogy” is a remastered compilation of his first three mixtapes: House of Balloons, Thursday, and Echoes of Silence. It captures The Weeknd’s early sound: moody, atmospheric R&B defined by vulnerability, escapism, and nocturnal storytelling.",
     cover: "images/trilogy.jpg",
     alt: "Trilogy album cover",
@@ -32,17 +32,20 @@ const albums = {
 function changeAlbum(key) {
   const album = albums[key];
 
-  // Hide the home content, show the album content
+  // hide home content, show  album content
   document.querySelector("#home-albumtitle-section").style.display = "none";
   document.querySelector("#album-content").style.display = "block";
 
-  // Update the content
+  // update title + description
   document.querySelector("#album-title").textContent = album.title;
   document.querySelector("#album-description").textContent = album.description;
 
-  const image = document.querySelector("#main-image");
+  // update album cover
+  const image = document.querySelector("#album-cover");
   image.src = album.cover;
   image.alt = album.alt;
 
+  // spotify embed
   document.querySelector("#spotify-embed").src = album.spotify;
 }
+
